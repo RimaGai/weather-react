394285a40d319c06496a503a4c721223556729d6
RimaGai <puolus@gmail.com>
2021-10-19 18:23:17 +0200

    Created app structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="Weather">
+          <div className="Form">
+            <form className="search-form">
+              <div className="row mb-3">
+                <div className="col-6">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter city"
+                    autoComplete="off"
+                  />
+                </div>
+                <div className="col-3">
+                  <input type="submit" value="Search" className="btn w-100" />
+                </div>
+                <div className="col-3">
+                  <button className="current-location btn w-100">
+                    Current
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div className="current">
+            <h1>Oslo</h1>
+            <ul>
+              <li>Thursday</li>
+              <li>Light rain</li>
+            </ul>
+          </div>
+          <div className="row Weather">
+            <div className="col-6">
+              <img
+                src="http://openweathermap.org/img/wn/09d@2x.png"
+                className="icon"
+                alt=""
+                width="140"
+              />
+              <span className="temperature"> 12</span>
+              <span className="units"> ºC</span>
+            </div>
+            <div className="col-6 conditions">
+              <ul>
+                <li>
+                  Humidity:40 <span className="humidity"></span>%
+                </li>
+                <li>
+                  Wind:2 <span className="wind"></span> m/s
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="Footer">
+        <span className="github-link">
+          {" "}
+          <a
+            href="https://github.com/RimaGai/WeatherProject"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            {" "}
+            Open source code
+          </a>{" "}
+          by Rima Gaileviciute
+        </span>
+      </div>
     </div>
   );
 }
